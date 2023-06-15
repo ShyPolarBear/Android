7c61235b339162f82d0bdee5943e220666bdbbb7
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-06-15 18:22:46 +0900

    feature: UiState 사용하는 Sample 추가

--- presentation/src/main/java/com/shypolarbear/presentation/sample/SampleViewModel.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/sample/SampleViewModel.kt
@@ -7,6 +7,10 @@ import com.shypolarbear.domain.model.ExampleModel
 import com.shypolarbear.domain.usecase.ExampleUseCase
 import com.shypolarbear.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,11 +22,32 @@ class SampleViewModel @Inject constructor(
     private val _sampleData = MutableLiveData<ExampleModel>()
     val sampleData: LiveData<ExampleModel> = _sampleData
 
+    private val _sampleState: MutableStateFlow<SampleState> = MutableStateFlow(SampleState(true, false, ""))
+    val sampleState: StateFlow<SampleState> = _sampleState.asStateFlow()
+
     fun loadSampleData() {
         viewModelScope.launch {
             val loadedSample = exampleUseCase.loadSampleData()
-            loadedSample.let {
-                _sampleData.postValue(it)
+//            loadedSample.let {
+//                _sampleData.postValue(it)
+//            }
+            when(loadedSample) {
+                null -> {
+                    _sampleState.update { state ->
+                        state.copy(
+                            loading = false,
+                            error = true,
+                        )
+                    }
+                }
+                else -> {
+                    _sampleState.update { state ->
+                        state.copy(
+                            loading = false,
+                            category = loadedSample.toString(),
+                        )
+                    }
+                }
             }
         }
     }
