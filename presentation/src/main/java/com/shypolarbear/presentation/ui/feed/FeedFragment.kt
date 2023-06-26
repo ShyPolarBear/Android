19b6ce8f3ccda6ae3b06a8d6118f00d205905d49
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-06-26 23:04:41 +0900

    refactor: 하드 코딩 제거, Power Menu 공통으로 사용할 수 있게 수정

--- presentation/src/main/java/com/shypolarbear/presentation/ui/feed/FeedFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/feed/FeedFragment.kt
@@ -1,24 +1,28 @@
 package com.shypolarbear.presentation.ui.feed
 
-import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
 import com.shypolarbear.presentation.databinding.FragmentFeedBinding
+import com.shypolarbear.presentation.util.PowerMenuUtil
+import com.skydoves.powermenu.PowerMenuItem
 import com.skydoves.powermenu.kotlin.powerMenu
 
 class FeedFragment: BaseFragment<FragmentFeedBinding, FeedViewModel> (
     R.layout.fragment_feed
 ){
     override val viewModel: FeedViewModel by viewModels()
-
-    private val moreMenu by powerMenu<FeedMenuFactory>()
+    private val feedSortItems: List<PowerMenuItem> = listOf(PowerMenuItem("최신"), PowerMenuItem("최근 인기"),PowerMenuItem("best"))
 
     override fun initView() {
 
         binding.feedToolbarSort.setOnClickListener {
-            moreMenu?.showAsDropDown(it)
-        }
 
+            PowerMenuUtil.getPowerMenu(
+                requireContext(),
+                viewLifecycleOwner,
+                feedSortItems
+            ) .showAsDropDown(binding.feedToolbarSort, -290, 0)
+        }
     }
 }
