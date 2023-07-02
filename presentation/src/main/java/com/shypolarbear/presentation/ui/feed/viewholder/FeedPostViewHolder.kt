93ce26279380f72f6717f3c6c041ce83e67f5438
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-07-03 02:35:45 +0900

    feature: 좋아요 버튼 클릭 시 버튼 색 변경 기능 추가

--- presentation/src/main/java/com/shypolarbear/presentation/ui/feed/viewholder/FeedPostViewHolder.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/feed/viewholder/FeedPostViewHolder.kt
@@ -1,10 +1,12 @@
 package com.shypolarbear.presentation.ui.feed.viewholder
 
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.tabs.TabLayoutMediator
 import com.shypolarbear.domain.model.feed.FeedPost
 import com.shypolarbear.domain.model.feed.FeedPostImg
+import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.databinding.ItemFeedBinding
 import com.shypolarbear.presentation.ui.feed.adapter.FeedPostImgAdapter
 import com.shypolarbear.presentation.util.PowerMenuUtil
@@ -18,6 +20,8 @@ class FeedPostViewHolder(
     companion object {
         private const val POWER_MENU_OFFSET_X = -290
         private const val POWER_MENU_OFFSET_Y = 0
+        private var IS_FEED_LIKE = false
+        private var IS_FEED_REPLY_LIKE = false
     }
 
     private val feedPostPropertyItems: List<PowerMenuItem> = listOf(PowerMenuItem("수정"), PowerMenuItem("삭제"), PowerMenuItem("신고"), PowerMenuItem("차단"))
@@ -37,6 +41,16 @@ class FeedPostViewHolder(
                 viewLifeCycleOwner,
                 feedPostPropertyItems
             ) .showAsDropDown(binding.ivFeedPostReplyProperty, POWER_MENU_OFFSET_X, POWER_MENU_OFFSET_Y)
+        }
+
+        binding.btnFeedPostLike.setOnClickListener {
+            IS_FEED_LIKE = !IS_FEED_LIKE
+            checkLike(IS_FEED_LIKE, binding, 0)
+        }
+
+        binding.btnFeedPostReplyLike.setOnClickListener {
+            IS_FEED_REPLY_LIKE = !IS_FEED_REPLY_LIKE
+            checkLike(IS_FEED_REPLY_LIKE, binding, 1)
         }
     }
 
@@ -61,6 +75,38 @@ class FeedPostViewHolder(
             ) { tab, position ->
 
             }.attach()
+        }
+    }
+
+    private fun checkLike(isLike: Boolean, binding: ItemFeedBinding, diff: Int) {
+
+        val on = ContextCompat.getDrawable(binding.root.context, R.drawable.ic_btn_like_on)
+        val off = ContextCompat.getDrawable(binding.root.context, R.drawable.ic_btn_like_off)
+
+        when (diff) {
+            0 -> {
+                if (isLike) {
+                    // 좋아요 누른 경우
+                    binding.btnFeedPostLike.background = on
+                } else {
+                    // 좋아요 취소한 경우
+                    binding.btnFeedPostLike.background = off
+                }
+            }
+
+            1 -> {
+                if (isLike) {
+                    // 좋아요 누른 경우
+                    binding.btnFeedPostReplyLike.background = on
+                } else {
+                    // 좋아요 취소한 경우
+                    binding.btnFeedPostReplyLike.background = off
+                }
+            }
+
+            else -> {
+
+            }
         }
     }
 }
