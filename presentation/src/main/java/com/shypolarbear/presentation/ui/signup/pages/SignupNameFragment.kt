bd5ac8799150a51bfa66efead3855fc778a32c14
kimmandoo <mingyu5675@gmail.com>
2023-07-06 15:35:23 +0900

    feat: 회원가입 ui page4까지 xml만 작성완료

--- presentation/src/main/java/com/shypolarbear/presentation/ui/signup/pages/SignupNameFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/signup/pages/SignupNameFragment.kt
@@ -2,9 +2,14 @@ package com.shypolarbear.presentation.ui.signup.pages
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.GradientDrawable
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
@@ -22,6 +27,7 @@ class SignupNameFragment :
         binding.apply {
             layoutSignupName.setOnTouchListener { v, _ ->
                 hideKeyboard()
+                customSetTextColor(tvSignupNameRule, R.color.Gray_05)
                 v.clearFocus()
                 false
             }
@@ -30,7 +36,9 @@ class SignupNameFragment :
             }
 
             etSignupNickname.setOnFocusChangeListener { v, hasFocus ->
-                tvSignupNameRule.isActivated = hasFocus
+                if (hasFocus) {
+                    customSetTextColor(tvSignupNameRule, R.color.Blue_02)
+                }
             }
 
             etSignupNickname.addTextChangedListener(object : TextWatcher {
@@ -40,18 +48,35 @@ class SignupNameFragment :
                     count: Int,
                     after: Int,
                 ) {
+                    tvSignupNameRule.text = getString(R.string.signup_check_text)
                 }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
+                    customSetTextColor(tvSignupNameRule, R.color.Blue_02)
                 }
 
                 override fun afterTextChanged(s: Editable?) {
+                    if (s!!.length !in 8 downTo 2) {
+                        tvSignupNameRule.text = getString(R.string.singup_error_text)
+                        customSetTextColor(tvSignupNameRule, R.color.Error_01)
+                    } else {
+                        tvSignupNameRule.text = getString(R.string.signup_confirm_text)
+                        customSetTextColor(tvSignupNameRule, R.color.Success_01)
+                    }
+                    if (s.isEmpty()) {
+                        tvSignupNameRule.text = getString(R.string.signup_name_rule)
+                        customSetTextColor(tvSignupNameRule, R.color.Blue_02)
+                    }
                 }
             })
         }
     }
-
+    private fun customSetTextColor(v: TextView, color: Int){
+        v.setTextColor(ContextCompat.getColor(
+            requireContext(),
+            color
+        ))
+    }
     private fun hideKeyboard() {
         if (activity != null && requireActivity().currentFocus != null) {
             val inputManager =
