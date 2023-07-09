e17b482a7ba5a6d0ec386b6e5a681fc90c744da1
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-07-09 15:53:25 +0900

    feature: 피드 자세히 보기 이미지 viewPager 구현

--- presentation/src/main/java/com/shypolarbear/presentation/ui/feed/feedDetail/FeedDetailFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/feed/feedDetail/FeedDetailFragment.kt
@@ -1,9 +1,12 @@
 package com.shypolarbear.presentation.ui.feed.feedDetail
 
 import androidx.fragment.app.viewModels
+import com.google.android.material.tabs.TabLayoutMediator
+import com.shypolarbear.domain.model.feed.FeedPostImg
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
 import com.shypolarbear.presentation.databinding.FragmentFeedDetailBinding
+import com.shypolarbear.presentation.ui.common.ImageViewPagerAdapter
 import com.shypolarbear.presentation.ui.feed.adapter.FeedPostAdapter
 import com.shypolarbear.presentation.ui.feed.feedDetail.adapter.FeedCommentAdapter
 import kotlinx.coroutines.NonDisposableHandle.parent
@@ -14,6 +17,26 @@ class FeedDetailFragment : BaseFragment<FragmentFeedDetailBinding, FeedDetailVie
     override val viewModel: FeedDetailViewModel by viewModels()
 
     override fun initView() {
+
+        with(binding.viewpagerFeedDetailImg) {
+            adapter = ImageViewPagerAdapter().apply {
+                submitList(
+                    // 테스트 데이터
+                    mutableListOf(
+                        FeedPostImg("https://github.com/ShyPolarBear/Android/assets/107917980/9690c7b7-2bde-498c-a5be-886b6e5b5405"),
+                        FeedPostImg("https://github.com/ShyPolarBear/Android/assets/107917980/4eed9944-8689-442e-87d7-c4ac6a939103"),
+                        FeedPostImg("https://github.com/ShyPolarBear/Android/assets/107917980/083a3008-642b-42b8-9845-6696aa641e31"),
+                        FeedPostImg("https://github.com/ShyPolarBear/Android/assets/107917980/4c0f2ca6-defc-455c-b384-ba573f72a981"),
+                        FeedPostImg("https://github.com/ShyPolarBear/Android/assets/107917980/24b65d18-006e-41ee-8440-2a5f00706028")
+                    )
+                )
+            }
+
+            TabLayoutMediator(binding.tablayoutFeedDetailIndicator, this
+            ) { tab, position ->
+
+            }.attach()
+        }
 
         viewModel.loadFeedComment()
         setFeedComment()
