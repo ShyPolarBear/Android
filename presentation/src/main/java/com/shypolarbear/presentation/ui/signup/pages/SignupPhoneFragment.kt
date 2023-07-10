8adbd8c0c4c52cb4a2b09e78a7ee29bc7d655a68
kimmandoo <mingyu5675@gmail.com>
2023-07-10 13:23:24 +0900

    feat: phonenumber 자동 대쉬

--- presentation/src/main/java/com/shypolarbear/presentation/ui/signup/pages/SignupPhoneFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/signup/pages/SignupPhoneFragment.kt
@@ -1,5 +1,9 @@
 package com.shypolarbear.presentation.ui.signup.pages
 
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
@@ -11,5 +15,25 @@ class SignupPhoneFragment: BaseFragment<FragmentSignupPhoneBinding, SignupViewMo
     override val viewModel: SignupViewModel by viewModels({ requireParentFragment() })
 
     override fun initView() {
+        binding.apply {
+            etSignupPhone.addTextChangedListener(object : PhoneNumberFormattingTextWatcher("KR"){
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                    super.beforeTextChanged(s, start, count, after)
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    super.afterTextChanged(s)
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    super.onTextChanged(s, start, before, count)
+                }
+            })
+        }
     }
 }
