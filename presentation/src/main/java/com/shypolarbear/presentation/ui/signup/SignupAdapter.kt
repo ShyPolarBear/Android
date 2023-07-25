9f852742a6ee5e3d2468c7d3cfc0c57566df258b
kimmandoo <mingyu5675@gmail.com>
2023-07-25 16:18:00 +0900

    feat: Viewpager2 setup, 인디케이터 설정, 키보드에 adjust_nothing 적용

---
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/signup/SignupAdapter.kt
@@ -0,0 +1,20 @@
+package com.shypolarbear.presentation.ui.signup
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+
+class SignupAdapter(fragmentActivity: FragmentActivity, private val fragmentList: List<Fragment>) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+}
--- presentation/src/main/java/com/shypolarbear/presentation/ui/signup/SignupFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/signup/SignupFragment.kt
@@ -1,34 +1,70 @@
 package com.shypolarbear.presentation.ui.signup
 
-import android.view.View.OnTouchListener
-import androidx.fragment.app.FragmentManager
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.fragment.app.viewModels
-import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
 import com.shypolarbear.presentation.databinding.FragmentSignupBinding
 import com.shypolarbear.presentation.ui.signup.pages.SignupMailFragment
-import com.shypolarbear.presentation.ui.signup.pages.SignupPhoneFragment
 import com.shypolarbear.presentation.ui.signup.pages.SignupNameFragment
+import com.shypolarbear.presentation.ui.signup.pages.SignupPhoneFragment
 import com.shypolarbear.presentation.ui.signup.pages.SignupTermsFragment
+import timber.log.Timber
 
 class SignupFragment :
     BaseFragment<FragmentSignupBinding, SignupViewModel>(R.layout.fragment_signup) {
 
     override val viewModel: SignupViewModel by viewModels()
+    private lateinit var pagerAdapter: SignupAdapter
+    private var idx = 1
 
     override fun initView() {
+        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
 
-//        childFragmentManager.beginTransaction().replace(R.id.signup_fragment, SignupTermsFragment()).commit()
-        childFragmentManager.beginTransaction().replace(R.id.signup_fragment, SignupNameFragment()).commit()
-//        childFragmentManager.beginTransaction().replace(R.id.signup_fragment, SignupMailFragment()).commit()
-//        childFragmentManager.beginTransaction().replace(R.id.signup_fragment, SignupPhoneFragment()).commit()
-
+        val pageList = listOf(
+            SignupTermsFragment(),
+            SignupNameFragment(),
+            SignupPhoneFragment(),
+            SignupMailFragment()
+        )
+        pagerAdapter = SignupAdapter(requireActivity(), pageList)
         binding.apply {
+            signupIndicator.text = getString(R.string.signup_page_indicator, idx)
+            signupViewpager.apply {
+                adapter = pagerAdapter
+                isUserInputEnabled = false
+            }
+
             signupBtnNext.setOnClickListener {
                 //조건을 갖추면 활성화 되도록
                 binding.signupBtnNext.isActivated = true
                 binding.signupTvNext.isActivated = true
+                if (signupTvNext.text.equals("가입 완료")) {
+                    // 메인페이지로 넘어가도록
+                } else {
+                    val currentItem = signupViewpager.currentItem
+
+                    if (currentItem < (signupViewpager.adapter!!.itemCount.minus(1) ?: 0)) {
+                        idx = currentItem + 2
+                        signupIndicator.text = getString(R.string.signup_page_indicator, idx)
+                        signupViewpager.setCurrentItem(currentItem + 1, true)
+                    }
+                    Timber.d("SIGN + $currentItem")
+                }
+            }
+
+            signupBtnBack.setOnClickListener {
+                val currentItem = signupViewpager.currentItem
+
+                if (currentItem > 0) {
+                    idx--
+                    signupIndicator.text = getString(R.string.signup_page_indicator, idx)
+                    signupViewpager.setCurrentItem(currentItem - 1, true)
+                    Timber.d("SIGN - $currentItem")
+                }
             }
         }
     }
