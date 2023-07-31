e9b04afdd1bdad1857d2e2eb05d68f6f324543e5
kimmandoo <mingyu5675@gmail.com>
2023-07-31 20:04:32 +0900

    feat: GlideUtil 추가

---
+++ presentation/src/main/java/com/shypolarbear/presentation/util/GlideUtil.kt
@@ -0,0 +1,41 @@
+package com.shypolarbear.presentation.util
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestListener
+
+
+object GlideUtil {
+    fun loadImage(context: Context, url: String, view: ImageView) {
+        initGlide(context, url).into(view)
+    }
+
+    fun loadImage(context: Context, url: String, view: ImageView, placeHolder: Int) {
+        initGlide(context, url).placeholder(placeHolder).into(view)
+    }
+
+    fun loadImage(
+        context: Context,
+        url: String,
+        view: ImageView,
+        listener: RequestListener<Drawable>,
+    ) {
+        initGlide(context, url).listener(listener).into(view)
+    }
+
+    fun loadImage(context: Context, uri: Uri, view: ImageView) {
+        initGlide(context, uri = uri).into(view)
+    }
+
+    fun loadCircleImage(context: Context, uri: Uri, view: ImageView) {
+        initGlide(context, uri = uri).centerCrop().circleCrop().into(view)
+    }
+
+    private fun initGlide(context: Context, url: String? = null, uri: Uri? = null) = run {
+        if (uri == null) Glide.with(context).load(url) else Glide.with(context).load(uri)
+    }
+
+}
