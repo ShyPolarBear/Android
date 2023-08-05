b5f0dcdc695830a075e365fb8d39061a7f533453
kimmandoo <mingyu5675@gmail.com>
2023-08-06 01:21:53 +0900

    feat: 중복검사로직 들어갈 곳 작성중

--- presentation/src/main/java/com/shypolarbear/presentation/ui/signup/pages/SignupNameFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/signup/pages/SignupNameFragment.kt
@@ -3,9 +3,11 @@ package com.shypolarbear.presentation.ui.signup.pages
 import android.annotation.SuppressLint
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
 import com.shypolarbear.presentation.databinding.FragmentSignupNameBinding
@@ -16,11 +18,16 @@ import com.shypolarbear.presentation.util.GlideUtil
 import com.shypolarbear.presentation.util.keyboardDown
 import com.shypolarbear.presentation.util.setColorStateWithInput
 import com.shypolarbear.presentation.util.setTextColorById
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SignupNameFragment :
     BaseFragment<FragmentSignupNameBinding, SignupViewModel>(R.layout.fragment_signup_name) {
     override val viewModel: SignupViewModel by viewModels({ requireParentFragment() })
 
+    lateinit var state: InputState
     private val pickMedia =
         registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
             uri?.let {
@@ -52,6 +59,11 @@ class SignupNameFragment :
                     after: Int,
                 ) {
                     tvSignupNameRule.text = getString(R.string.signup_check_text)
+                    etSignupNickname.setColorStateWithInput(
+                        InputState.ON,
+                        tvSignupNameRule,
+                        signupEtCheck
+                    )
                 }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
@@ -64,28 +76,59 @@ class SignupNameFragment :
                 }
 
                 override fun afterTextChanged(s: Editable?) {
-                    val state = when {
+                    state = when {
                         s != null && s.length !in NAME_RANGE -> {
+                            pgSignupNameCheck.visibility = View.INVISIBLE
                             tvSignupNameRule.text = getString(R.string.singup_error_text)
                             InputState.ERROR
                         }
 
                         s.isNullOrEmpty() -> {
+                            pgSignupNameCheck.visibility = View.INVISIBLE
                             tvSignupNameRule.text = getString(R.string.signup_name_rule)
                             InputState.ON
                         }
 
                         else -> {
-                            tvSignupNameRule.text = getString(R.string.signup_confirm_text)
+                            pgSignupNameCheck.visibility = View.VISIBLE
                             InputState.ACCEPT
                         }
                     }
-                    etSignupNickname.setColorStateWithInput(
-                        state,
-                        tvSignupNameRule,
-                        signupEtCheck
-                    )
-                    viewModel.setNameData(s.toString())
+
+                    lifecycleScope.launch {
+                        viewModel.setNameData("")
+                        if(state == InputState.ACCEPT){
+                            delay(1500)
+                            val isValid = withContext(Dispatchers.IO) {
+                                // 검증 로직
+                                s!!.length == 5
+                            }
+                            tvSignupNameRule.text = getString(R.string.signup_check_text)
+
+                            state = if (isValid) {
+                                pgSignupNameCheck.visibility = View.INVISIBLE
+                                tvSignupNameRule.text = getString(R.string.signup_confirm_text)
+                                viewModel.setNameData(s.toString())
+                                InputState.ACCEPT
+                            } else {
+                                pgSignupNameCheck.visibility = View.INVISIBLE
+                                tvSignupNameRule.text = getString(R.string.signup_exists_text)
+                                InputState.ERROR
+                            }
+                            etSignupNickname.setColorStateWithInput(
+                                state,
+                                tvSignupNameRule,
+                                signupEtCheck
+                            )
+                        }else{
+                            etSignupNickname.setColorStateWithInput(
+                                state,
+                                tvSignupNameRule,
+                                signupEtCheck
+                            )
+                        }
+                    }
+
                 }
             })
         }
