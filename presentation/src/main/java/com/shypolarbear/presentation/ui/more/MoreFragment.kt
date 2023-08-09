b343e679bb0eaae321ca2e3713696b4697f41a56
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-08-09 16:37:25 +0900

    feature: 메인-더보기 ui 구현

--- presentation/src/main/java/com/shypolarbear/presentation/ui/more/MoreFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/more/MoreFragment.kt
@@ -1,6 +1,8 @@
 package com.shypolarbear.presentation.ui.more
 
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
 import com.shypolarbear.presentation.databinding.FragmentMoreBinding
@@ -12,6 +14,7 @@ class MoreFragment: BaseFragment<FragmentMoreBinding, MoreViewModel> (
     override val viewModel: MoreViewModel by viewModels()
 
     override fun initView() {
-
+        val bottomNavigationViewMainActivity = requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
+        bottomNavigationViewMainActivity.isVisible = true
     }
 }
