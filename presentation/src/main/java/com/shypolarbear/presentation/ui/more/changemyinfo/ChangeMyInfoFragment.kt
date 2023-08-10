b0623f6dc7ce8249470be92c82e689d17914bd69
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-08-10 23:48:28 +0900

    feature: 내 정보 수정 -> 메인-더보기 이동 구현

--- presentation/src/main/java/com/shypolarbear/presentation/ui/more/changemyinfo/ChangeMyInfoFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/more/changemyinfo/ChangeMyInfoFragment.kt
@@ -1,6 +1,9 @@
 package com.shypolarbear.presentation.ui.more.changemyinfo
 
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
 import com.shypolarbear.presentation.databinding.FragmentChangeMyInfoBinding
@@ -12,6 +15,13 @@ class ChangeMyInfoFragment: BaseFragment<FragmentChangeMyInfoBinding, ChangeMyIn
     override val viewModel: ChangeMyInfoViewModel by viewModels()
 
     override fun initView() {
+        val bottomNavigationViewMainActivity = requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
+        bottomNavigationViewMainActivity.isVisible = false
 
+        binding.apply {
+            btnChangeMyInfoBack.setOnClickListener {
+                findNavController().navigate(R.id.action_changeMyInfoFragment_to_navigation_more)
+            }
+        }
     }
 }
