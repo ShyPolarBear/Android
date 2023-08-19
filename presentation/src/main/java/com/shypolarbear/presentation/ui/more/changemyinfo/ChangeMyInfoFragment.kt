c67e50d3a9698f6ff6d19553c516aa5463b02579
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-08-19 20:06:59 +0900

    feat: 닉네임 글자 수 검사 동작 추가

--- presentation/src/main/java/com/shypolarbear/presentation/ui/more/changemyinfo/ChangeMyInfoFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/more/changemyinfo/ChangeMyInfoFragment.kt
@@ -1,5 +1,7 @@
 package com.shypolarbear.presentation.ui.more.changemyinfo
 
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -7,6 +9,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
 import com.shypolarbear.presentation.databinding.FragmentChangeMyInfoBinding
+import com.shypolarbear.presentation.ui.signup.NAME_RANGE
+import com.shypolarbear.presentation.util.InputState
+import com.shypolarbear.presentation.util.keyboardDown
+import com.shypolarbear.presentation.util.setColorStateWithInput
 
 class ChangeMyInfoFragment: BaseFragment<FragmentChangeMyInfoBinding, ChangeMyInfoViewModel> (
     R.layout.fragment_change_my_info
@@ -21,6 +27,62 @@ class ChangeMyInfoFragment: BaseFragment<FragmentChangeMyInfoBinding, ChangeMyIn
         binding.apply {
             btnChangeMyInfoBack.setOnClickListener {
                 findNavController().navigate(R.id.action_changeMyInfoFragment_to_navigation_more)
+            }
+
+            edtChangeMyInfoNickname.apply {
+                keyboardDown(this@ChangeMyInfoFragment)
+                addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        count: Int,
+                        after: Int,
+                    ) {
+                        setColorStateWithInput(
+                            InputState.ON,
+                            tvChangeMyInfoNameRule,
+                            ivChangeMyInfoNameCheck
+                        )
+                    }
+
+                    override fun onTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        before: Int,
+                        count: Int,
+                    ) {
+                        setColorStateWithInput(
+                            InputState.ON,
+                            tvChangeMyInfoNameRule,
+                            ivChangeMyInfoNameCheck
+                        )
+                    }
+
+                    override fun afterTextChanged(s: Editable?) {
+                        val state = when {
+                            s.isNullOrEmpty() -> {
+                                tvChangeMyInfoNameRule.text = getString(R.string.signup_name_rule)
+                                InputState.ON
+                            }
+
+                            s.length !in NAME_RANGE -> {
+                                tvChangeMyInfoNameRule.text = getString(R.string.singup_error_text)
+                                InputState.ERROR
+                            }
+
+                            else -> {
+                                tvChangeMyInfoNameRule.text = getString(R.string.signup_confirm_text)
+                                InputState.ACCEPT
+                            }
+                        }
+
+                        setColorStateWithInput(
+                            state,
+                            tvChangeMyInfoNameRule,
+                            ivChangeMyInfoNameCheck
+                        )
+                    }
+                })
             }
         }
     }
