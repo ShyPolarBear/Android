aa9c33a4337e7869ef2ed1af7e708643de2e1535
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-08-20 18:18:33 +0900

    feat: 수정 버튼 클릭 시 각 값의 조건 확인하는 동작 추가

--- presentation/src/main/java/com/shypolarbear/presentation/ui/more/changemyinfo/ChangeMyInfoFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/more/changemyinfo/ChangeMyInfoFragment.kt
@@ -3,6 +3,7 @@ package com.shypolarbear.presentation.ui.more.changemyinfo
 import android.telephony.PhoneNumberFormattingTextWatcher
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -18,6 +19,7 @@ import com.shypolarbear.presentation.util.emailPattern
 import com.shypolarbear.presentation.util.keyboardDown
 import com.shypolarbear.presentation.util.phonePattern
 import com.shypolarbear.presentation.util.setColorStateWithInput
+import timber.log.Timber
 
 class ChangeMyInfoFragment: BaseFragment<FragmentChangeMyInfoBinding, ChangeMyInfoViewModel> (
     R.layout.fragment_change_my_info
@@ -25,6 +27,9 @@ class ChangeMyInfoFragment: BaseFragment<FragmentChangeMyInfoBinding, ChangeMyIn
 
     override val viewModel: ChangeMyInfoViewModel by viewModels()
     private lateinit var phoneNumber: String
+    private var nameState: InputState = InputState.OFF
+    private var phoneNumberState: InputState = InputState.OFF
+    private var emailState: InputState = InputState.OFF
 
     override fun initView() {
         val bottomNavigationViewMainActivity = requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
@@ -36,8 +41,18 @@ class ChangeMyInfoFragment: BaseFragment<FragmentChangeMyInfoBinding, ChangeMyIn
             }
 
             btnChangeMyInfoRevise.setOnClickListener {
-                // TODO("내 정보 수정 조건 검사 완료 후 이전 페이지로 이동")
-                findNavController().navigate(R.id.action_changeMyInfoFragment_to_navigation_more)
+
+                if (nameState == InputState.ERROR || phoneNumberState == InputState.ERROR || emailState == InputState.ERROR) {
+                    Toast.makeText(requireContext(), getString(R.string.check_my_info_term), Toast.LENGTH_SHORT).show()
+                }
+                else if (nameState == InputState.OFF && phoneNumberState == InputState.OFF && emailState == InputState.OFF) {
+                    Toast.makeText(requireContext(), getString(R.string.check_my_info_input), Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    // TODO("수정 된 정보 서버로 전달")
+                    findNavController().navigate(R.id.action_changeMyInfoFragment_to_navigation_more)
+                }
+
             }
 
             edtChangeMyInfoNickname.apply {
@@ -73,16 +88,19 @@ class ChangeMyInfoFragment: BaseFragment<FragmentChangeMyInfoBinding, ChangeMyIn
                         val state = when {
                             s.isNullOrEmpty() -> {
                                 tvChangeMyInfoNameRule.text = getString(R.string.signup_name_rule)
+                                nameState = InputState.ON
                                 InputState.ON
                             }
 
                             s.length !in NAME_RANGE -> {
                                 tvChangeMyInfoNameRule.text = getString(R.string.singup_error_text)
+                                nameState = InputState.ERROR
                                 InputState.ERROR
                             }
 
                             else -> {
                                 tvChangeMyInfoNameRule.text = getString(R.string.signup_confirm_text)
+                                nameState = InputState.ACCEPT
                                 InputState.ACCEPT
                             }
                         }
@@ -103,18 +121,21 @@ class ChangeMyInfoFragment: BaseFragment<FragmentChangeMyInfoBinding, ChangeMyIn
                     val state = when {
                         s.isNullOrEmpty() -> {
                             tvChangeMyInfoPhoneNumberRule.text = getString(R.string.signup_phone_hint_detail)
+                            phoneNumberState = InputState.ON
                             InputState.ON
                         }
 
                         s.length == PHONE_NUMBER_DASH_INCLUDE -> {
                             phoneNumber = s.replace(phonePattern, "")
                             tvChangeMyInfoPhoneNumberRule.text = getString(R.string.signup_phone_hint_confirm)
+                            phoneNumberState = InputState.ACCEPT
                             InputState.ACCEPT
                         }
 
                         else -> {
                             phoneNumber = ""
                             tvChangeMyInfoPhoneNumberRule.text = getString(R.string.signup_phone_hint_error)
+                            phoneNumberState = InputState.ERROR
                             InputState.ERROR
                         }
                     }
@@ -133,16 +154,19 @@ class ChangeMyInfoFragment: BaseFragment<FragmentChangeMyInfoBinding, ChangeMyIn
 
                     val state = when {
                         it.isNullOrEmpty() -> {
+                            emailState = InputState.ON
                             InputState.ON
                         }
 
                         match == null -> {
                             tvChangeMyInfoEmailRule.text = getString(R.string.signup_mail_hint_error)
+                            emailState = InputState.ERROR
                             InputState.ERROR
                         }
 
                         else -> {
                             tvChangeMyInfoEmailRule.text = getString(R.string.signup_mail_hint_confirm)
+                            emailState = InputState.ACCEPT
                             InputState.ACCEPT
                         }
                     }
