2febd99fb1da6f85db7bf49d52db918cf0a4ef46
kimmandoo <mingyu5675@gmail.com>
2023-09-14 14:11:17 +0900

    refactor: test 파일들 삭제

--- presentation/src/main/java/com/shypolarbear/presentation/ui/login/LoginFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/login/LoginFragment.kt
@@ -45,8 +45,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>(
             code?.let {
                 when (it) {
                     SIGNUP_NEED -> findNavController().navigate(sendTokenToJoin)
-//                    LOGIN_SUCCESS -> findNavController().navigate(R.id.action_loginFragment_to_quizMainFragment)
-                    LOGIN_SUCCESS -> findNavController().navigate(R.id.action_loginFragment_to_imageTestFragment)
+                    LOGIN_SUCCESS -> findNavController().navigate(R.id.action_loginFragment_to_quizMainFragment)
                     LOGIN_FAIL -> {
                         setVisibilityInvert(
                             binding.btnClickedLogin,
