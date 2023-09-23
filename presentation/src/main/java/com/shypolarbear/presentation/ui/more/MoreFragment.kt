c70a64fd5d7a366412d21365cc7399ac5546042c
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-09-24 02:43:15 +0900

    feat: 더보기 화면에서 서버에서 받아온 내 프로필 사진, 닉네임 데이터 연동

--- presentation/src/main/java/com/shypolarbear/presentation/ui/more/MoreFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/more/MoreFragment.kt
@@ -5,6 +5,7 @@ import androidx.navigation.fragment.findNavController
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
 import com.shypolarbear.presentation.databinding.FragmentMoreBinding
+import com.shypolarbear.presentation.util.GlideUtil
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,6 +18,15 @@ class MoreFragment: BaseFragment<FragmentMoreBinding, MoreViewModel> (
     override fun initView() {
 
         binding.apply {
+            viewModel.getMyInfo()
+            viewModel.myInfo.observe(viewLifecycleOwner) { info ->
+                tvMoreUserNickname.setText(info.nickName)
+                if (!info.profileImage.isNullOrBlank()) {
+                    GlideUtil.loadImage(requireContext(), info.profileImage, binding.ivMoreUserProfile)
+                } else {
+                    GlideUtil.loadImage(requireContext(), url = null, view = binding.ivMoreUserProfile, placeHolder = R.drawable.ic_user_base_profile)
+                }
+            }
 
             layoutMoreMyInfoChange.setOnClickListener {
                 findNavController().navigate(R.id.action_navigation_more_to_changeMyInfoFragment)
