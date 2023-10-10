12a3ecabe5d5395e2a4b129c4bede7554038e8ec
kimmandoo <mingyu5675@gmail.com>
2023-10-10 15:02:11 +0900

    feat: xml 작성

--- presentation/src/main/java/com/shypolarbear/presentation/ui/ranking/RankingViewModel.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/ranking/RankingViewModel.kt
@@ -1,11 +1,15 @@
 package com.shypolarbear.presentation.ui.ranking
 
+import com.shypolarbear.domain.usecase.ranking.LoadMyRankingUseCase
+import com.shypolarbear.domain.usecase.ranking.LoadTotalRankingUseCase
 import com.shypolarbear.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class RankingViewModel @Inject constructor(
-
+    private val loadMyRankingUseCase: LoadMyRankingUseCase,
+    private val loadTotalRankingUseCase: LoadTotalRankingUseCase
 ) : BaseViewModel() {
+
 }
