4627daf9db7c57852c016bded64a2e2bb29276e3
kimmandoo <mingyu5675@gmail.com>
2023-10-10 15:48:26 +0900

    feat: recyclerview adapter 작성 및 무한 스크롤 연결, ViewModel 작성

---
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/ranking/RankingAdapter.kt
@@ -0,0 +1,76 @@
+package com.shypolarbear.presentation.ui.ranking
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.shypolarbear.domain.model.ranking.Ranking
+import com.shypolarbear.presentation.R
+import com.shypolarbear.presentation.databinding.ItemFeedLoadingBinding
+import com.shypolarbear.presentation.databinding.ItemRankingBinding
+import com.shypolarbear.presentation.ui.mypage.adapter.LoadingViewHolder
+import com.shypolarbear.presentation.util.GlideUtil
+import com.shypolarbear.presentation.util.MyFeedType
+
+class RankingAdapter(private val _items: List<Ranking>) :
+    ListAdapter<Ranking, RecyclerView.ViewHolder>(RankingDiffCallback()) {
+
+    inner class ItemRankingViewHolder(private val binding: ItemRankingBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bindItems(myRanking: Ranking) {
+            binding.apply {
+                tvRankingRank.text = myRanking.rank.toString()
+                GlideUtil.loadImage(binding.root.context, myRanking.profileImage, ivRankingProfile)
+                tvRankingName.text = myRanking.nickName
+                tvRankingPoint.text = binding.root.context.getString(R.string.ranking_point_value, myRanking.point)
+                tvRankingPossible.text = binding.root.context.getString(R.string.ranking_possible_value, myRanking.winningPercent)
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (_items[position] != null) MyFeedType.ITEM.state else MyFeedType.LOADING.state
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == MyFeedType.ITEM.state) {
+            ItemRankingViewHolder(
+                ItemRankingBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        } else {
+            LoadingViewHolder(
+                ItemFeedLoadingBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ItemRankingViewHolder) {
+            holder.bindItems(_items[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return _items.size
+    }
+}
+
+class RankingDiffCallback : DiffUtil.ItemCallback<Ranking>() {
+
+    override fun areItemsTheSame(oldItem: Ranking, newItem: Ranking): Boolean {
+        return oldItem.rankingId == newItem.rankingId
+    }
+
+    override fun areContentsTheSame(oldItem: Ranking, newItem: Ranking): Boolean {
+        return oldItem == newItem
+    }
+}
--- presentation/src/main/java/com/shypolarbear/presentation/ui/ranking/RankingFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/ranking/RankingFragment.kt
@@ -1,9 +1,15 @@
 package com.shypolarbear.presentation.ui.ranking
 
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
 import com.shypolarbear.presentation.databinding.FragmentRankingBinding
+import com.shypolarbear.presentation.ui.mypage.FeedContentType
+import com.shypolarbear.presentation.util.GlideUtil
+import com.shypolarbear.presentation.util.infiniteScroll
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,6 +17,39 @@ class RankingFragment :
     BaseFragment<FragmentRankingBinding, RankingViewModel>(R.layout.fragment_ranking) {
     override val viewModel: RankingViewModel by viewModels()
     override fun initView() {
+        viewModel.loadRankingData()
 
+        viewModel.myRankingResponse.observe(viewLifecycleOwner) { myRanking ->
+            myRanking?.let {
+                binding.apply {
+                    tvRankingRank.text = myRanking.rank.toString()
+                    GlideUtil.loadImage(requireContext(), myRanking.profileImage, ivRankingProfile)
+                    tvRankingName.text = myRanking.nickName
+                    tvRankingPoint.text = getString(R.string.ranking_point_value, myRanking.point)
+                    tvRankingPossible.text =
+                        getString(R.string.ranking_possible_value, myRanking.winningPercent)
+                }
+            }
+        }
+
+        viewModel.totalRankingResponse.observe(viewLifecycleOwner) { totalRanking ->
+            totalRanking?.let {
+                binding.apply {
+                    rankingProgressbar.isVisible = false
+                    setAdapter(RankingAdapter(totalRanking.content))
+                }
+            }
+        }
+
+        binding.apply {
+            rankingProgressbar.isVisible = true
+        }
+    }
+
+    private fun setAdapter(adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>) {
+        binding.rvRanking.adapter = adapter
+        binding.rvRanking.infiniteScroll {
+            viewModel.loadMoreRanking()
+        }
     }
 }
