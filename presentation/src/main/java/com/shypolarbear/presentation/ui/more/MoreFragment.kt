4224ff334dc1ff2323433327c04b859d9d5c91ad
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-11-07 00:31:47 +0900

    feat: 문의하기, 오픈 소스 라이브러리 클릭 동작 구현

--- presentation/src/main/java/com/shypolarbear/presentation/ui/more/MoreFragment.kt
+++ presentation/src/main/java/com/shypolarbear/presentation/ui/more/MoreFragment.kt
@@ -1,12 +1,12 @@
 package com.shypolarbear.presentation.ui.more
 
+import android.content.Intent
+import android.net.Uri
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.shypolarbear.presentation.R
 import com.shypolarbear.presentation.base.BaseFragment
 import com.shypolarbear.presentation.databinding.FragmentMoreBinding
-import com.shypolarbear.presentation.ui.feed.feedTotal.FeedTotalFragmentDirections
-import com.shypolarbear.presentation.ui.feed.feedTotal.WriteChangeDivider
 import com.shypolarbear.presentation.ui.more.dialog.LogOutDialog
 import com.shypolarbear.presentation.util.GlideUtil
 import dagger.hilt.android.AndroidEntryPoint
@@ -58,6 +58,22 @@ class MoreFragment: BaseFragment<FragmentMoreBinding, MoreViewModel> (
                     viewModel.requestLogout()
 
                 }
+            }
+
+            layoutMoreOpenSourceLibrary.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(requireContext().getString(R.string.more_open_source_library_link)))
+                startActivity(intent)
+            }
+
+            layoutMoreContact.setOnClickListener {
+                val email = Intent(Intent.ACTION_SEND)
+                val beomjunEmail = requireContext().getString(R.string.Beomjun_Email)
+
+                email.data = Uri.parse(requireContext().getString(R.string.more_contact_email_data))
+                email.type = requireContext().getString(R.string.more_contact_email_type)
+                email.putExtra(Intent.EXTRA_EMAIL, arrayOf(beomjunEmail))
+                email.putExtra(Intent.EXTRA_TEXT, requireContext().getString(R.string.more_contact_email_content))
+                startActivity(email)
             }
 
             viewModel.loginState.observe(viewLifecycleOwner) {
